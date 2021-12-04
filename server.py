a5f1dd453c93a182e959bff8ae7199f5c4eebb14
lucidbard <lucidbard@users.noreply.github.com>
2021-12-04 15:44:59 -0500

    December 4, 2021 3:44 PM

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ def index():
             if data.filename:
                 data.save(os.path.join(FOLDER, data.filename))
             live_prediction = LivePredictions(
-                file=data.filename)
+                file=os.path.join(FOLDER, data.filename))
             result = live_prediction.make_predictions()
         return result
 
